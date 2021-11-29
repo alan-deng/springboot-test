6fc2f7ebad52e2c83449db3238a618ecfdc425ca
jmalabed <jmalabed27@gmail.com>
2021-11-29 10:15:21 -0800

    learning maven and installing json, getting json response from server

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.json.simple.JSONObject;
 
 @SpringBootApplication
 @RestController
@@ -13,9 +14,11 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
+// make calculation function that will take in parameters
 	@GetMapping("/ddd") // link here is equivalent of app.get("linkName") in express
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) { //@requestParam is parameters in link
-		return String.format("Hello %s!", name);
+	public JSONObject hello(@RequestParam(value = "name", defaultValue = "World") String name) { //@requestParam is parameters in line
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("key1","val1");
+		return jsonObject;
 	}
 }
